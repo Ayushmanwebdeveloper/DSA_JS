c81d5ae7509f7f52a659e692c2a2b238ce6219e5
Ayushman <ayushmantripathi7724@gmail.com>
2023-02-14 20:16:33 +0530

    update

--- BST.js
+++ BST.js
@@ -1,3 +1,10 @@
+//           10
+//       6         15
+//     3   8          20
+
+
+
+
 class Node {
     constructor(value){
         this.value = value;
@@ -65,7 +72,7 @@ class BinarySearchTree {
         }
         return false;
     }
- BFS(){
+    BFS(){
         var node = this.root,
             data = [],
             queue = [];
@@ -79,21 +86,50 @@ class BinarySearchTree {
         }
         return data;
     }
+    DFSPreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSPostOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        console.log(data);
+        return data;
+    }
+    DFSInOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 
-//      10
-//   5     13
-// 2  7  11  16
-
 var tree = new BinarySearchTree();
-tree.insert(10)
-tree.insert(5)
-tree.insert(13)
-tree.insert(11)
-tree.insert(2)
-tree.insert(16)
-tree.insert(7)
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
+tree.DFSPreOrder();
+tree.DFSPostOrder();
+tree.DFSInOrder();
 
 
